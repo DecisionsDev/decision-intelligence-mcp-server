5674cff35df5a98d18213f9f7c89a43de56db521
Orsini Jean-Philippe <jporsini@li-a1e779cc-315f-11b2-a85c-e6a503a5417e.ibm.com>
2025-07-18 02:34:18 +0200

    discover all operations of a decision

--- src/index.ts
+++ src/index.ts
@@ -24,7 +24,7 @@ function getParameters(jsonSchema:any): parametersType {
     return params;
 }
 
-async function registerTool(server: McpServer, apikey: string, baseURL: string, decisionId: string, operationName: string) {
+async function registerTool(server: McpServer, apikey: string, baseURL: string, decisionId: string) {
     var decisionOpenAPI = await getDecisionOpenapi(apikey, baseURL, decisionId);
     debug("openapi", JSON.stringify(decisionOpenAPI, null, " "));
 
@@ -33,39 +33,37 @@ async function registerTool(server: McpServer, apikey: string, baseURL: string, 
         const operationId = value.post.operationId;
         debug("path info", value);
 
-        if (operationId === operationName) {
-            debug("Found operationName", key);
+        debug("Found operationName", key);
 
-            var operation = value.post.requestBody.content["application/json"];
-            var inputSchema = operation.schema;
-            debug("operation", operation);
-            debug("inputSchema", inputSchema);
+        var operation = value.post.requestBody.content["application/json"];
+        var inputSchema = operation.schema;
+        debug("operation", operation);
+        debug("inputSchema", inputSchema);
 
-            var operationJsonInputSchema = expandJSONSchemaDefinition(inputSchema, decisionOpenAPI.components.schemas)
-            debug("operationJsonSchema after expand", JSON.stringify(operationJsonInputSchema, null, " "));
+        var operationJsonInputSchema = expandJSONSchemaDefinition(inputSchema, decisionOpenAPI.components.schemas)
+        debug("operationJsonSchema after expand", JSON.stringify(operationJsonInputSchema, null, " "));
 
-            // WO does not support white spaces for tool names
-            const toolName = (decisionOpenAPI.info.title + " " + operationName).replaceAll(" ", "_");
+        // WO does not support white spaces for tool names
+        const toolName = (value.post.summary + " " + operationId).replaceAll(" ", "_");
 
-            const inputParameters:any = getParameters(operationJsonInputSchema);
+        const inputParameters:any = getParameters(operationJsonInputSchema);
 
-            server.registerTool(
-                toolName,
-                {
-                    title: value.post.summary,
-                    description: value.post.description,
-                    inputSchema: inputParameters
-                },
-                async (input, n) => {
-                    var decInput = input;
-                    debug("Execute decision with", JSON.stringify(decInput, null, " "))
-                    var str = await executeDecision(apikey, baseURL, decisionId, operationName, decInput);
-                    return {
-                        content: [{ type: "text", text: str}]
-                    };
-                }
-            );
-        }
+        server.registerTool(
+            toolName,
+            {
+                title: value.post.summary,
+                description: value.post.description,
+                inputSchema: inputParameters
+            },
+            async (input, n) => {
+                var decInput = input;
+                debug("Execute decision with", JSON.stringify(decInput, null, " "))
+                var str = await executeDecision(apikey, baseURL, decisionId, operationId, decInput);
+                return {
+                    content: [{ type: "text", text: str}]
+                };
+            }
+        );
     }
 }
 
@@ -78,22 +76,20 @@ const server = new McpServer({
 
 var args = process.argv.slice(2);
 
-if (args.length != 4) {
-    console.error("USAGE: <APIKEY> <BASE_URL> <DECISION_ID> <OPERATION>");
+if (args.length != 3) {
+    console.error("USAGE: <APIKEY> <BASE_URL> <DECISION_ID>");
     process.exit(1);
 }
   
 var apikey = args[0];
 var baseURL = args[1];
 var decisionId = args[2];
-var operation = args[3];
 
 debug("APIKEY=" + apikey);
 debug("BASEURL=" + baseURL);
 debug("DECISION_ID=" + decisionId);
-debug("OPERATION=" + operation);
 
-await registerTool(server, apikey, baseURL, decisionId, operation);
+await registerTool(server, apikey, baseURL, decisionId);
 
 const transport = new StdioServerTransport();
 await server.connect(transport);
