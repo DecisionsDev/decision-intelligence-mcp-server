e2175e67960146c693114c114db0dd63716510fc
Jean-Philippe Orsini <jeanfi@gmail.com>
2025-07-29 21:52:55 +0200

    enable any check

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -4,6 +4,5 @@ import tseslint from 'typescript-eslint';
 
 export default tseslint.config(
     eslint.configs.recommended,
-    tseslint.configs.recommended,
-    {rules: {"@typescript-eslint/no-explicit-any": "warn" }}
+    tseslint.configs.recommended
 );
