0b5ce425b19683a2729fba6598473a6c9e6af0b5
Benjamin Ratiarisolo <benjamin.ratiarisolo@fr.ibm.com>
2025-07-29 14:11:51 +0200

    Describe DecisionRuntime test

--- tests/decision-runtime.test.ts
+++ tests/decision-runtime.test.ts
@@ -1,17 +1,23 @@
 import { DecisionRuntime, parseDecisionRuntime } from '../src/decision-runtime.js';
 
-test('Properly parse "DI" as DecisionRuntime.DI', () => {
-    expect(parseDecisionRuntime("DI")).toEqual(DecisionRuntime.DI);
-});
+describe('Decision Runtime', () => {
 
-test('Properly parse "ADS" as DecisionRuntime.ADS', () => {
-    expect(parseDecisionRuntime("ADS")).toEqual(DecisionRuntime.ADS);
-});
+    describe('parseDecisionRuntime', () => {
 
-test('Parsing "ODM" returns undefined', () => {
-    expect(parseDecisionRuntime("ODM")).toEqual(undefined);
-});
+        test('should parse "DI" as DecisionRuntime.DI', () => {
+            expect(parseDecisionRuntime("DI")).toEqual(DecisionRuntime.DI);
+        });
 
-test('Parsing "odm" returns undefined', () => {
-    expect(parseDecisionRuntime("odm")).toEqual(undefined);
+        test('should parse "ADS" as DecisionRuntime.ADS', () => {
+            expect(parseDecisionRuntime("ADS")).toEqual(DecisionRuntime.ADS);
+        });
+
+        test('should parse  "ODM" returns undefined', () => {
+            expect(parseDecisionRuntime("ODM")).toEqual(undefined);
+        });
+
+        test('should parse  "odm" returns undefined', () => {
+            expect(parseDecisionRuntime("odm")).toEqual(undefined);
+        });
+    });
 });
