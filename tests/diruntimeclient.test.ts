d4aaec7d8dac8c72475c863c05c7eceef5a9169b
Benjamin Ratiarisolo <benjamin.ratiarisolo@fr.ibm.com>
2025-08-12 14:03:00 +0200

    Implement basic authentication support to connect to decision runtime

--- tests/diruntimeclient.test.ts
+++ tests/diruntimeclient.test.ts
@@ -1,9 +1,4 @@
-import {
-    getDecisionMetadata,
-    getDecisionServiceIds,
-    getDecisionServiceOpenAPI,
-    getMetadata
-} from '../src/diruntimeclient.js';
+import { getDecisionMetadata, getDecisionServiceIds, getDecisionServiceOpenAPI, getMetadata } from '../src/diruntimeclient';
 import nock from 'nock';
 import loanvalidationOpenapi from '../tests/loanvalidation-openapi.json';
 import {Configuration} from "../src/command-line.js";
@@ -33,7 +28,14 @@ const metadata =  [{
 }];
 
 const url = 'https://example.com';
-const configuration = new Configuration( 'apiKey', DecisionRuntime.DI, undefined, url, '1.2.3', false);
+const apikey = 'apiKey';
+const version = '1.2.3';
+const runtime = DecisionRuntime.DI;
+const apiKeyConfiguration = new Configuration(new Credentials({apikey : apikey}), runtime, undefined, url, version, false);
+const username = 'username';
+const password = 'password';
+const encodedUsernamePassword= Buffer.from(`${username}:${password}`).toString('base64');
+const basicAuthConfiguration = new Configuration(new Credentials({username: username, password: password}), runtime, undefined, url, version, false);
 const deploymentSpaceId = 'toto';
 const decisionId = 'toto'
 const decisionMetadata = {
@@ -49,40 +51,42 @@ const decisionMetadata = {
 
 nock(url)
     .get('/deploymentSpaces/development/metadata?names=decisionServiceId')
+    .matchHeader('authorization', `Basic ${encodedUsernamePassword}`)
     .reply(200, metadata)
     .get('/deploymentSpaces/notexist/metadata?names=decisionServiceId')
     .reply(404)
     .get('/selectors/lastDeployedDecisionService/deploymentSpaces/development/openapi?decisionServiceId=ID1&outputFormat=JSON/openapi')
+    .matchHeader('apikey', apikey)
     .replyWithFile(200, 'tests/loanvalidation-openapi.json')
     .get(`/deploymentSpaces/${deploymentSpaceId}/decisions/${decisionId}/metadata`)
-    .reply(200, decisionMetadata)
-;
+    .matchHeader('authorization', `Basic ${encodedUsernamePassword}`)
+    .reply(200, decisionMetadata);
 
 test('getDecisionServiceIds', () => {
     expect(getDecisionServiceIds(metadata)).toEqual(["ID1", "ID2"]);
 });
 
 test('getMetadata', async () => {
-    return getMetadata(configuration, 'development')
+    return getMetadata(basicAuthConfiguration, 'development')
         .then(data => {
             expect(data).toEqual(metadata);
     });
 });
 
 test('getMetadata with not exist deploymentSpace', async () => {
-    await expect(getMetadata(configuration, 'notexist'))
+    await expect(getMetadata(apiKeyConfiguration, 'notexist'))
         .rejects.toThrow('Request failed with status code 404');
 });
 
 test('getDecisionServiceOpenAPI', async() => {
-    return getDecisionServiceOpenAPI(configuration, 'ID1')
+    return getDecisionServiceOpenAPI(apiKeyConfiguration, 'ID1')
         .then(data => {
             expect(data).toEqual(loanvalidationOpenapi);
         })
 });
 
 test('getDecisionMetadata', async () => {
-    return getDecisionMetadata(configuration, deploymentSpaceId, decisionId)
+    return getDecisionMetadata(basicAuthConfiguration, deploymentSpaceId, decisionId)
         .then(data => {
             expect(data).toEqual(decisionMetadata);
         })
